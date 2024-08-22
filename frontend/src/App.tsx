34537b488297760760140cb895db8b1aa4db54d2
ElemarLeonel <elemarleonelbalduino@gmail.com>
2024-08-21 20:25:35 -0400

    refac: Remove comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React from "react";
 import TaskForm from "./components/TaskForm";
 import TaskDisplay from "./components/TaskDisplay";
--- frontend/src/components/TaskForm.tsx
+++ frontend/src/components/TaskForm.tsx
@@ -1,4 +1,3 @@
-// src/components/TaskForm.tsx
 import React, { useState } from "react";
 import api from "../services/api";
 
